9f71bc3a7df4e83c53768d77c37978f627b4b8a1
Expert <Expert@FUCK-YOU>
2022-03-03 19:38:44 +0100

    Projektdateien hinzufügen.

---
+++ 17 und 4/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace _17_und_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_wkarte = new System.Windows.Forms.Button();
+            this.lbl_sieger = new System.Windows.Forms.Label();
+            this.lbl_spieler = new System.Windows.Forms.Label();
+            this.lbl_dealer = new System.Windows.Forms.Label();
+            this.btn_ende = new System.Windows.Forms.Button();
+            this.tb_risk = new System.Windows.Forms.TrackBar();
+            this.lbl_risk = new System.Windows.Forms.Label();
+            this.lbl_risk2 = new System.Windows.Forms.Label();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.lbl_dealerende = new System.Windows.Forms.Label();
+            this.lbl_spielereinzelkarten = new System.Windows.Forms.Label();
+            this.lbl_dealereinzelkarten = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_risk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_wkarte
+            // 
+            this.btn_wkarte.BackgroundImage = global::_17_und_4.Properties.Resources.Button;
+            this.btn_wkarte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_wkarte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_wkarte.Location = new System.Drawing.Point(550, 9);
+            this.btn_wkarte.Name = "btn_wkarte";
+            this.btn_wkarte.Size = new System.Drawing.Size(104, 167);
+            this.btn_wkarte.TabIndex = 0;
+            this.btn_wkarte.Text = "Karte ziehen";
+            this.btn_wkarte.UseVisualStyleBackColor = true;
+            this.btn_wkarte.Click += new System.EventHandler(this.btn_wkarte_Click);
+            // 
+            // lbl_sieger
+            // 
+            this.lbl_sieger.AutoSize = true;
+            this.lbl_sieger.Location = new System.Drawing.Point(12, 9);
+            this.lbl_sieger.Name = "lbl_sieger";
+            this.lbl_sieger.Size = new System.Drawing.Size(0, 25);
+            this.lbl_sieger.TabIndex = 5;
+            // 
+            // lbl_spieler
+            // 
+            this.lbl_spieler.AutoSize = true;
+            this.lbl_spieler.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_spieler.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_spieler.Location = new System.Drawing.Point(97, 199);
+            this.lbl_spieler.Name = "lbl_spieler";
+            this.lbl_spieler.Size = new System.Drawing.Size(106, 25);
+            this.lbl_spieler.TabIndex = 6;
+            this.lbl_spieler.Text = "Spieler: 00";
+            // 
+            // lbl_dealer
+            // 
+            this.lbl_dealer.AutoSize = true;
+            this.lbl_dealer.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_dealer.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_dealer.Location = new System.Drawing.Point(322, 199);
+            this.lbl_dealer.Name = "lbl_dealer";
+            this.lbl_dealer.Size = new System.Drawing.Size(102, 25);
+            this.lbl_dealer.TabIndex = 7;
+            this.lbl_dealer.Text = "Dealer: 00";
+            this.lbl_dealer.Visible = false;
+            // 
+            // btn_ende
+            // 
+            this.btn_ende.BackgroundImage = global::_17_und_4.Properties.Resources.Button;
+            this.btn_ende.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_ende.Location = new System.Drawing.Point(550, 358);
+            this.btn_ende.Name = "btn_ende";
+            this.btn_ende.Size = new System.Drawing.Size(104, 39);
+            this.btn_ende.TabIndex = 8;
+            this.btn_ende.Text = "Ende";
+            this.btn_ende.UseVisualStyleBackColor = true;
+            this.btn_ende.Click += new System.EventHandler(this.btn_ende_Click);
+            // 
+            // tb_risk
+            // 
+            this.tb_risk.BackColor = System.Drawing.Color.Black;
+            this.tb_risk.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.tb_risk.Location = new System.Drawing.Point(3, 57);
+            this.tb_risk.Maximum = 21;
+            this.tb_risk.Minimum = 1;
+            this.tb_risk.Name = "tb_risk";
+            this.tb_risk.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tb_risk.Size = new System.Drawing.Size(56, 340);
+            this.tb_risk.TabIndex = 9;
+            this.tb_risk.Value = 16;
+            this.tb_risk.Scroll += new System.EventHandler(this.tb_risk_Scroll);
+            // 
+            // lbl_risk
+            // 
+            this.lbl_risk.AutoSize = true;
+            this.lbl_risk.BackColor = System.Drawing.Color.Black;
+            this.lbl_risk.Font = new System.Drawing.Font("Miriam Mono CLM", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lbl_risk.ForeColor = System.Drawing.Color.White;
+            this.lbl_risk.Location = new System.Drawing.Point(-2, 3);
+            this.lbl_risk.Name = "lbl_risk";
+            this.lbl_risk.Size = new System.Drawing.Size(125, 30);
+            this.lbl_risk.TabIndex = 10;
+            this.lbl_risk.Text = "Risiko:";
+            this.lbl_risk.Click += new System.EventHandler(this.lbl_risk_Click);
+            // 
+            // lbl_risk2
+            // 
+            this.lbl_risk2.AutoSize = true;
+            this.lbl_risk2.BackColor = System.Drawing.Color.Black;
+            this.lbl_risk2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_risk2.ForeColor = System.Drawing.Color.White;
+            this.lbl_risk2.Location = new System.Drawing.Point(-1, 30);
+            this.lbl_risk2.Name = "lbl_risk2";
+            this.lbl_risk2.Size = new System.Drawing.Size(52, 20);
+            this.lbl_risk2.TabIndex = 11;
+            this.lbl_risk2.Text = "(1-21)";
+            this.lbl_risk2.Click += new System.EventHandler(this.lbl_risk2_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.BackgroundImage = global::_17_und_4.Properties.Resources.Button;
+            this.btn_show.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_show.Location = new System.Drawing.Point(550, 185);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(104, 167);
+            this.btn_show.TabIndex = 12;
+            this.btn_show.Text = "Show cards";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // lbl_dealerende
+            // 
+            this.lbl_dealerende.AutoSize = true;
+            this.lbl_dealerende.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_dealerende.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dealerende.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_dealerende.Location = new System.Drawing.Point(75, 140);
+            this.lbl_dealerende.Name = "lbl_dealerende";
+            this.lbl_dealerende.Size = new System.Drawing.Size(469, 36);
+            this.lbl_dealerende.TabIndex = 13;
+            this.lbl_dealerende.Text = "Dealer möchte nichtmehr ziehen";
+            this.lbl_dealerende.Visible = false;
+            // 
+            // lbl_spielereinzelkarten
+            // 
+            this.lbl_spielereinzelkarten.AutoSize = true;
+            this.lbl_spielereinzelkarten.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_spielereinzelkarten.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_spielereinzelkarten.Location = new System.Drawing.Point(98, 264);
+            this.lbl_spielereinzelkarten.Name = "lbl_spielereinzelkarten";
+            this.lbl_spielereinzelkarten.Size = new System.Drawing.Size(76, 25);
+            this.lbl_spielereinzelkarten.TabIndex = 14;
+            this.lbl_spielereinzelkarten.Text = "Karten:";
+            // 
+            // lbl_dealereinzelkarten
+            // 
+            this.lbl_dealereinzelkarten.AutoSize = true;
+            this.lbl_dealereinzelkarten.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_dealereinzelkarten.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_dealereinzelkarten.Location = new System.Drawing.Point(330, 264);
+            this.lbl_dealereinzelkarten.Name = "lbl_dealereinzelkarten";
+            this.lbl_dealereinzelkarten.Size = new System.Drawing.Size(76, 25);
+            this.lbl_dealereinzelkarten.TabIndex = 15;
+            this.lbl_dealereinzelkarten.Text = "Karten:";
+            this.lbl_dealereinzelkarten.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::_17_und_4.Properties.Resources.points;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 63);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::_17_und_4.Properties.Resources.border;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(67, 354);
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackgroundImage = global::_17_und_4.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(660, 404);
+            this.Controls.Add(this.lbl_dealereinzelkarten);
+            this.Controls.Add(this.lbl_spielereinzelkarten);
+            this.Controls.Add(this.lbl_dealerende);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.lbl_risk2);
+            this.Controls.Add(this.lbl_risk);
+            this.Controls.Add(this.btn_ende);
+            this.Controls.Add(this.lbl_dealer);
+            this.Controls.Add(this.lbl_spieler);
+            this.Controls.Add(this.lbl_sieger);
+            this.Controls.Add(this.btn_wkarte);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tb_risk);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "17 und 4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tb_risk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_wkarte;
+        private System.Windows.Forms.Label lbl_sieger;
+        private System.Windows.Forms.Label lbl_spieler;
+        private System.Windows.Forms.Label lbl_dealer;
+        private System.Windows.Forms.Button btn_ende;
+        private System.Windows.Forms.TrackBar tb_risk;
+        private System.Windows.Forms.Label lbl_risk;
+        private System.Windows.Forms.Label lbl_risk2;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Label lbl_dealerende;
+        private System.Windows.Forms.Label lbl_spielereinzelkarten;
+        private System.Windows.Forms.Label lbl_dealereinzelkarten;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ 17 und 4/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_und_4
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<int> L1 = new List<int>() { 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4 };
+        List<int> L2 = new List<int>();
+        List<int> L3 = new List<int>();
+
+        int p1_Card = 0;     //Deklariert die Karten des Spieler
+        int com_Card = 0;      //Deklariert die Karten des Computers
+
+        int p1_cardValue = 0;       //Deklariert die Zufällige Karte des Spielers
+        int com_cardValue = 0;     //Deklariert die Zufällige Karte des Computers
+
+        string show_P1Card = "Karten: ";      //deklarierung des Textes der einzelnen Spielerkarten
+        string show_ComCard = "karten: ";       //deklarierung des Textes der einzelnen Dealerkarten
+
+        bool p1_ass = false;     //Prüft, ob der Spieler ein Ass gezogen hat
+        bool com_ass = false;        //Prüft, ob der Computer ein Ass gezogen hatt
+
+        public Form1()
+        {
+             InitializeComponent();
+            Random zufallsZahl = new Random();
+            p1_cardValue = zufallsZahl.Next(0, 
+                
+            L1.Count());     //Wählen einer zufälligen Karte, aus dem Stapel "L1"
+
+            L2.Add(L1[p1_cardValue]);      //diese zufällige Karte wird auf die Spielerhand hinzugefügt
+            show_P1Card += L1[p1_cardValue];        //Die Karte wird dem Spieler bei seinen einzelnen karten angerechnet
+            lbl_spielereinzelkarten.Text = show_P1Card;       //Die einzeilkarte wird dem Label hinzugefügt
+            p1_Card += L1[p1_cardValue];       //der Wert der Karte wird dem Spieler auf seine insgesammten Punkte angerechnet
+            L1.RemoveAt(p1_cardValue);        //diese zufällige Karte wird von dem Kartenstapel entfernt
+
+            p1_cardValue = zufallsZahl.Next(0, L1.Count());     //das ganze nochmal, da man mit 2 karten startet
+            L2.Add(L1[p1_cardValue]);
+            show_P1Card += ", " + L1[p1_cardValue];
+            lbl_spielereinzelkarten.Text = show_P1Card;
+            p1_Card += L1[p1_cardValue];
+            L1.RemoveAt(p1_cardValue);
+
+            lbl_spieler.Text = "Spieler: " + p1_Card;        //der insgesammte Kartenwert des Spielers wird dem label hinzugefügt
+
+
+
+            com_cardValue = zufallsZahl.Next(0, 
+
+               L1.Count());       //Das gesammte wird nochmal für den  Dealer gemacht
+
+            L3.Add(L1[com_cardValue]);
+            show_ComCard += L1[com_cardValue];
+            lbl_dealereinzelkarten.Text = show_ComCard;
+            com_Card += L1[com_cardValue];
+            L1.RemoveAt(com_cardValue);
+
+            com_cardValue = zufallsZahl.Next(0, L1.Count());
+            L3.Add(L1[com_cardValue]);
+            show_ComCard += ", " + L1[com_cardValue];
+            lbl_dealereinzelkarten.Text = show_ComCard;
+            com_Card += L1[com_cardValue];
+            L1.RemoveAt(com_cardValue);
+
+            lbl_dealer.Text = "Dealer: " + com_Card;
+        }
+
+
+        private void btn_wkarte_Click(object sender, EventArgs e)
+        {
+            Random randomCard = new Random();
+            p1_cardValue = randomCard.Next(0, L1.Count());     //eine zufällige Karte aus derdem Kartenstapel wird gewählt
+
+            L2.Add(L1[p1_cardValue]);      //diese zufällige Karte wird auf die Spielerhand hinzugefügt
+            if (p1_cardValue == 1)      //wenn ein ass gezogen wird, wird spielerass auf true gesetzt
+            {
+                p1_ass = true;
+            }
+            show_P1Card += ", " + L1[p1_cardValue];     //Die Karte wird dem Spieler bei seinen einzelnen karten angerechnet
+            lbl_spielereinzelkarten.Text = show_P1Card;       //Die einzeilkarte wird dem Label hinzugefügt
+            p1_Card += L1[p1_cardValue];       //der Wert der Karte wird dem Spieler auf seine insgesammten Punkte angerechnet
+            L1.RemoveAt(p1_cardValue);        //diese zufällige Karte wird von dem Kartenstapel entfernt
+
+            lbl_spieler.Text = "Spieler: " + p1_Card;        //der insgesammte Kartenwert des Spielers wird dem label hinzugefügt
+
+
+            if (com_Card < tb_risk.Value)      //solange der insgesammte Kartenwert des Dealers unter dem Risikowert liegt, werden die folgebefehle (dieselben wie beim Spieler nur für den Dealer) ausgeführt
+            {
+                Random randomCom = new Random();
+                com_cardValue = randomCom.Next(0, L1.Count());
+
+                L3.Add(L1[com_cardValue]);
+                if (com_cardValue == 1)
+                {
+                    com_ass = true;
+                }
+                show_ComCard += ", " + L1[com_cardValue];
+                lbl_dealereinzelkarten.Text = show_ComCard;
+                com_Card += L1[com_cardValue];
+                L1.RemoveAt(com_cardValue);
+
+                lbl_dealer.Text = "Dealer: " + com_Card;
+                               
+            }
+            else        //wenn die Bedingung nicht zutrifft wird angezeigt das der Dealer nichtmehr ziehen möchte
+            {
+                lbl_dealerende.Visible = true;
+            }
+
+            if (p1_Card > 21 || com_Card <= 21 && L3.Count == 5)      //wenn der Spieler über 21 Punkte hat oder der dealer 5 karten hat und trozdem under oder gleich 21 Punkte hat, wird:
+            {
+                btn_wkarte.Enabled = false;     // die möglichkeit weitere Karten zu ziehen wird entzogen
+                btn_ende.Enabled = false;       // die möglichkeit ende zu drücken wird entzogen
+                lbl_sieger.Text = "Der Dealer hat gewonnen";        // Siegesanzeige 
+            
+            }
+            if (com_Card > 21 || p1_Card <= 21 && L2.Count == 5)     //wenn der Dealer über 21 Punkte hat oder der Spieler 5 karten hat und trozdem under oder gleich 21 Punkte hat, wird:
+            {
+                btn_wkarte.Enabled = false;     // die möglichkeit weitere Karten zu ziehen wird entzogen
+                btn_ende.Enabled = false;       // die möglichkeit ende zu drücken wird entzogen
+                lbl_sieger.Text = "Der Spieler hat gewonnen";       // Siegesanzeige
+
+            }
+
+        }
+
+        private void btn_ende_Click(object sender, EventArgs e)
+        {
+            btn_wkarte.Enabled = false;     // die möglichkeit ende zu drücken wird entzogen
+
+            if (p1_ass == true)     // wenn der Spieler ein ass hat wird ihm 10 hinzugefügt, esseiden der Spieler würde dann über 21 kommen
+            {
+                p1_Card += 10;
+                if (p1_Card > 21)
+                {
+                    p1_Card -= 10;
+                }
+                p1_ass = false;
+            }
+
+            while (com_cardValue < tb_risk.Value)
+            {
+                Random randomCom = new Random();
+                com_cardValue = randomCom.Next(0, L1.Count());     // der Dealer zieht solange karten wie der Kartenwert unter dem Risikowert liegt
+
+                L3.Add(L1[com_cardValue]);     //der Rest ist wieder wie bei jedem anderen codeblock bei dem karten gezogen werden für den Dealer
+                show_ComCard += ", " + L1[com_cardValue];
+                lbl_dealereinzelkarten.Text = show_ComCard;
+                com_Card+= L1[com_cardValue];
+                L1.RemoveAt(com_cardValue);
+
+                if (com_ass == true)      // wenn der Dealer ein ass hat wird ihm 10 hinzugefügt, esseiden der Dealer würde dann über 21 kommen
+                {
+                    com_Card+= 10;
+                    if (com_Card> 21)
+                    {
+                        com_Card -= 10;
+                    }
+                    com_ass = false;
+                }
+
+                lbl_dealer.Text = "Dealer: " + show_ComCard;       //der insgesammte Kartenwert des Dealer wird dem label hinzugefügt
+            }
+
+            lbl_dealer.Visible = true;      //da das Spiel jetzt vorbei ist, wird der Punktestand vom Dealer sichtbar gemacht
+            lbl_dealereinzelkarten.Visible = true;
+
+            if (p1_Card <= 21 && p1_Card > com_Card) { lbl_sieger.Text = "Der Spieler hat gewonnen"; }       // alle möglichen Siegesanzeigen, jenachdem wieviele Punkte der Spieler und der Dealer hat
+            if (com_Card <= 21 && p1_Card < com_Card) { lbl_sieger.Text = "Der Dealer hat gewonnen"; }
+            if (com_Card > 21) { lbl_sieger.Text = "Der Spieler hat gewonnen"; }
+            if (p1_Card > 21) { lbl_sieger.Text = "Der Dealer hat gewonnen"; }
+            if (com_Card == p1_Card || com_Card > 21 && p1_Card > 21) { lbl_sieger.Text = "Niemand hat gewonnen"; }
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)     //bietet die möglichkeit wärend der Runde die Karten des Dealers sichtbar oder unsichtbar zu schalten (für die cheater ;) )
+        {
+            lbl_dealer.Visible = !lbl_dealer.Visible;
+            lbl_dealereinzelkarten.Visible = !lbl_dealereinzelkarten.Visible;
+        }
+
+        private void lbl_risk2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_risk_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_risk_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
